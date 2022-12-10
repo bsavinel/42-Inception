b53155ac9683a5372912a4328029f60e5387ff84
bsavinel <bsavinel@student.42.fr>
2022-12-10 16:07:04 +0100

    commencement wp cli

---
+++ srcs/requirements/wordpress/script_wp.sh
@@ -0,0 +1,13 @@
+wp core download	--locale=php8 \
+					--version='latest' \
+
+wp core install		--admin_user=\
+					--admin_password=\
+					--admin_email=\
+					--url=\
+					--skip-email
+
+wp config create	--dbname=\
+					--dbname=
+
+# wp user create   surement utile pour metre mettre le nouvel use non admin
