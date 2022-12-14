8f04611784ef23c7df88a6ca9263faa6d410aa9e
bsavinel <bsavinel@student.42.fr>
2022-12-14 16:35:55 +0100

    add protection in sh script

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-#setup launch of mysql
-mysql_install_db  --datadir=/var/lib/mysql --user=mysql --auth-root-authentication-method=normal
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
+if [ ! -f $PROTECT_FILE ]
+then
+	#setup launch of mysql
+	mysql_install_db  --datadir=/var/lib/mysql --user=mysql --auth-root-authentication-method=normal
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
 
-#launch mysql in back ground 
-mysqld &
-while !(mysqladmin ping > /dev/null)
-do
-    sleep 5
-done
+	#launch mysql in back ground 
+	mysqld &
+	while !(mysqladmin ping > /dev/null)
+	do
+	    sleep 5
+	done
 
-#setup database in mysql
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@localhost IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@localhost IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_WORDPRESS;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD';"
-mysql -e "FLUSH PRIVILEGES;"
+	#setup database in mysql
+	mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@localhost IDENTIFIED BY '$DB_PASSWORD';"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@localhost IDENTIFIED BY '$DB_PASSWORD';"
+	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_WORDPRESS;"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD';"
+	mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin shutdown
+	mysqladmin shutdown
+	touch $PROTECT_FILE
+else
+	echo "Database already created"
+fi
--- srcs/requirements/wordpress/script_wp.sh
+++ srcs/requirements/wordpress/script_wp.sh
@@ -1,19 +1,25 @@
-wp core download	--allow-root
+#!/bin/sh
+if [ ! -f $PROTECT_FILE ]
+then
+	wp core download	--allow-root
 
-wp config create	--allow-root\
-					--dbname=$DB_NAME\
-					--dbuser=$DB_USER\
-					--dbpass=$DB_PASSWORD\
-					--dbhost="mariadb"\
-					--path="/var/www/wordpress/"
+	wp config create	--allow-root\
+						--dbname=$DB_NAME\
+						--dbuser=$DB_USER\
+						--dbpass=$DB_PASSWORD\
+						--dbhost="mariadb"\
+						--path="/var/www/wordpress/"
 
-wp core install		--allow-root
-					--admin_user=$ADMIN_USER\
-					--admin_password=$ADMIN_PASSWORD\
-					--admin_email=$ADMIN@example.com\
-					--url=localhost\
-					--title="Inception"\
-					--skip-email
+	wp core install		--allow-root
+						--admin_user=$ADMIN_USER\
+						--admin_password=$ADMIN_PASSWORD\
+						--admin_email=$ADMIN@example.com\
+						--url=localhost\
+						--title="Inception"\
+						--skip-email
 
-
+	touch $PROTECT_FILE
+else
+	echo "worpress is already installed an parametrized"
+fi
 # wp user create   surement utile pour metre mettre le nouvel use non admin
