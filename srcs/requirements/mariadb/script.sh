b1f8a87af074045b2cd3563f1050a812f5f3cb39
bsavinel <bsavinel@student.42.fr>
2022-12-19 10:17:45 +0100

    wp marche

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -17,22 +17,11 @@ then
 	echo "Database is ready"
 
 	#setup database in mysql
-	echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-
-	echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 	mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-
-	echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-
-	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-
-	#echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '$ROOT_PASSWORD';"
 	#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '$ROOT_PASSWORD';"
-
-	echo "FLUSH PRIVILEGES;"
 	mysql -e "FLUSH PRIVILEGES;"
 
 	mysqladmin shutdown
--- srcs/requirements/wordpress/script_wp.sh
+++ srcs/requirements/wordpress/script_wp.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 if [ ! -f $PROTECT_FILE ]
 then
-	sleep 15
 	wp core download	--allow-root --path="/var/www/wordpress/"
 
 	wp config create	--allow-root\
